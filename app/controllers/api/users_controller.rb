60183541ec001f55919b7edd3a8b1398ecca1314
mengna <linmengna9392@gmail.com>
2019-05-03 09:54:32 -0700

    testing

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -5,7 +5,8 @@ class Api::UsersController < ApplicationController
             login(@user)
             render "api/users/show"
         else
-            render json: @user.errors.full_messages, status: 422
+            render json:["user signup error"] , status: 401
+            # @user.errors.full_messages
         end
     end   
 
